7ec07670b840875d543181be79ebdb84f67c5a83
Son JunHyung <kingjh1125@gmail.com>
2024-01-12 09:27:59 +0900

    :sparkles: : add manageStudent

--- src/main/java/campManage/view/InputView.java
+++ src/main/java/campManage/view/InputView.java
@@ -11,11 +11,16 @@ public class InputView {
     private static final String INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값을 입력하였습니다. 다시 입력해주세요.";
     private static final int INPUT_START_RANGE = 1;
     private static final int MANAGE_MENU_END_RANGE = 3;
+    private static final int MANAGE_STUDENT_END_RANGE = 5;
 
 
 
     public int manageMenu() {
         return getInputWithValidation(this::readUserInput, this::validateManageMenuRange);
+    }
+
+    public int manageStudent() {
+        return getInputWithValidation(this::readUserInput, this::validateManageStudentRange);
     }
 
     private <T>T getInputWithValidation(IntSupplier inputSupplier, Function<Integer, T> function) {
@@ -33,6 +38,13 @@ public class InputView {
     private int validateManageMenuRange(int manageMenuNumber) {
         if (INPUT_START_RANGE <= manageMenuNumber && manageMenuNumber <= MANAGE_MENU_END_RANGE) {
             return manageMenuNumber;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private int validateManageStudentRange(int manageStudentNumber) {
+        if (INPUT_START_RANGE <= manageStudentNumber && manageStudentNumber <= MANAGE_STUDENT_END_RANGE) {
+            return manageStudentNumber;
         }
         throw new IllegalArgumentException();
     }
--- src/main/java/campManage/view/OutputView.java
+++ src/main/java/campManage/view/OutputView.java
@@ -14,4 +14,20 @@ public class OutputView {
             "관리 항목을 선택하세요...");
     }
 
+    public void manageStudent() {
+        System.out.println(SEPARATE_LINE);
+        System.out.println(
+            "1. 수강생 등록\n" +
+                "2. 수강생 조회\n" +
+                "3. 수강생 수정\n" +
+                "4. 수강생 삭제\n" +
+                "5. 돌아가기\n" +
+
+                "관리 항목을 선택하세요...");
+    }
+
+    public void backToManageMenu() {
+        System.out.println("이전 메뉴로 돌아갑니다.");
+    }
+
 }
