6f129b2b8d93822ad595f21a1021a5bd561c08f6
Son JunHyung <kingjh1125@gmail.com>
2024-01-13 12:27:21 +0900

    :recycle: : use functional Interface

--- src/main/java/campManage/view/InputView.java
+++ src/main/java/campManage/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntSupplier;
 import java.util.regex.Pattern;
@@ -14,46 +15,90 @@ public class InputView {
 
     private static final String INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값을 입력하였습니다. 다시 입력해주세요.";
     private static final String NAME_ERROR_MESSAGE = "[ERROR] 잘못된 입력입니다. 공백을 제외한 2글자 이상 10글자 이하의 한글로 입력해 주세요.";
-    private static final String SUBJECT_ERROR_MESSAGE =
+    private static final String REQUIRE_SUBJECT_ERROR_MESSAGE =
         "[ERROR] 잘못된 입력입니다. 1 이상 5 이하의 정수로 서로 다른 3개 이상의 필수 과목을 선택해주세요.\n" +
             "각 과목은 공백으로 구분되어야 합니다.";
+
+    private static final String OPTIONAL_SUBJECT_ERROR_MESSAGE =
+        "[ERROR] 잘못된 입력입니다. 1 이상 4 이하의 정수로 서로 다른 2개 이상의 선택 과목을 선택해주세요.\n" +
+            "각 과목은 공백으로 구분되어야 합니다.";
+    private static final String STATE_ERROR_MESSAGE = "[ERROR] 잘못된 입력입니다. 1 이상 3 이하의 정수로 입력해주세요.";
     private static final int INPUT_START_RANGE = 1;
     private static final int MANAGE_MENU_END_RANGE = 3;
     private static final int MANAGE_STUDENT_END_RANGE = 5;
     private static final int MANAGE_SCORE_END_RANGE = 4;
-    private static final int STATE_END_RANGE = 3;
 
     private static final Pattern NAME_PATTERN = Pattern.compile("^[가-힣]{2,10}$");
     private static final Pattern REQUIRE_SUBJECT_PATTERN = Pattern.compile("([1-5]\\s){2,}[1-5]");
+    private static final Pattern OPTIONAL_SUBJECT_PATTERN = Pattern.compile("([1-4]\\s){1,}[1-5]");
 
-    public int manageMenu() {
-        return getInputWithValidation(this::readUserInput, this::validateManageMenuRange);
-    }
-
-    public int manageStudent() {
-        return getInputWithValidation(this::readUserInput, this::validateManageStudentRange);
-    }
-
-    public int manageScore() {
-        return getInputWithValidation(this::readUserInput, this::validateManageScoreRange);
-    }
-
-
-    private <T> T getInputWithValidation(IntSupplier inputSupplier, Function<Integer, T> function) {
+    private <T> T getInputWithValidation(Function<Integer, T> function, String errorMessage) {
         while (true) {
             try {
-                int input = inputSupplier.getAsInt();
+                int input = readUserInput();
                 return function.apply(input);
             } catch (IllegalArgumentException e) {
-                System.out.println(INPUT_ERROR_MESSAGE);
+                System.out.println(errorMessage);
             }
         }
     }
 
+    public List<Subject> readSubjects(Consumer<String> consumer,
+        Function<Integer, Subject> function, String errorMessage) {
+        while (true) {
+            List<Subject> subjects = new ArrayList<>();
+            try {
+                String input = readLineInput();
+                consumer.accept(input);
+
+                List<String> subjectNumbers = Arrays.stream(input.split(" ")).toList();
+                validateIsDuplicate(subjectNumbers);
+
+                for (String subjectNumber : subjectNumbers) {
+                    subjects.add(function.apply(Integer.parseInt(subjectNumber)));
+                }
+                return subjects;
+            } catch (IllegalArgumentException e) {
+                System.out.println(errorMessage);
+            }
+        }
+    }
+
+    private int validateRange(int inputValue, int endRange) {
+        if (INPUT_START_RANGE <= inputValue && inputValue <= endRange) {
+            return inputValue;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void validatePattern(Pattern pattern, String input) {
+        if (!pattern.matcher(input).matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int manageMenu() {
+        return getInputWithValidation(this::validateManageMenuRange, INPUT_ERROR_MESSAGE);
+    }
+
+    public int manageStudent() {
+        return getInputWithValidation(this::validateManageStudentRange, INPUT_ERROR_MESSAGE);
+    }
+
+    public int manageScore() {
+        return getInputWithValidation(this::validateManageScoreRange, INPUT_ERROR_MESSAGE);
+    }
+
+    public State state() {
+        Function<Integer, State> stateMapper = State::getStateByInput;
+        return getInputWithValidation(stateMapper, STATE_ERROR_MESSAGE);
+    }
+
+
     public String name() {
         while (true) {
             try {
-                String input = readUserNameInput();
+                String input = readLineInput();
                 validateName(input);
                 return input;
             } catch (IllegalArgumentException e) {
@@ -63,123 +108,48 @@ public class InputView {
     }
 
     public List<Subject> requireSubject() {
-        while (true) {
-            List<Subject> subjects = new ArrayList<>();
-            try {
-                String input = readSubjectInput();
-                validateRequireSubject(input);
-
-                List<String> requireSubjectNumbers = Arrays.stream(input.split(" ")).toList();
-                validateIsDuplicate(requireSubjectNumbers);
-
-                for (String requireSubjectNumber : requireSubjectNumbers) {
-                    subjects.add(Subject.getRequireSubjectByOrdinal(Integer.parseInt(requireSubjectNumber)));
-                }
-                return subjects;
-            } catch (IllegalArgumentException e) {
-                System.out.println(SUBJECT_ERROR_MESSAGE);
-            }
-        }
+        Function<Integer, Subject> subjectMapper = Subject::getRequireSubjectByOrdinal;
+        return readSubjects(this::validateRequireSubject, subjectMapper,
+            REQUIRE_SUBJECT_ERROR_MESSAGE);
     }
-
-    private void validateIsDuplicate(List<String> requireSubjectNumbers) {
-        if(!(requireSubjectNumbers.stream().distinct().count() == requireSubjectNumbers.size())){
-            throw new IllegalArgumentException();
-        }
-    }
-
-//    public List<Subject> requireSubject() {
-//        while (true) {
-//            List<Subject> subjects = new ArrayList<>();
-//            try {
-//                String input = readSubjectInput();
-//                String[] numbers = input.split(" ");
-//                // vlidate(numbers);
-//                for (String number : numbers) {
-//                    subjects.add(Subject.getRequireSubjectByOrdinal(Integer.parseInt(number)));
-//                }
-//                return subjects;
-//            } catch (IllegalArgumentException e) {
-//                System.out.println(SUBJECT_ERROR_MESSAGE);
-//            }
-//        }
-//    }
 
     public List<Subject> optionalSubject() {
-        while (true) {
-            List<Subject> subjects = new ArrayList<>();
-            try {
-                String input = readSubjectInput();
-                String[] numbers = input.split(" ");
-                // vlidate(numbers);
-                for (String number : numbers) {
-                    subjects.add(Subject.getOptionalSubjectByOrdinal(Integer.parseInt(number)));
-                }
-                return subjects;
-            } catch (IllegalArgumentException e) {
-                System.out.println(SUBJECT_ERROR_MESSAGE);
-            }
-        }
-    }
-
-    public State state() {
-        while (true) {
-            try {
-                int input = readUserInput();
-                return State.getStateByInput(input);
-            } catch (IllegalArgumentException e) {
-                System.out.println(NAME_ERROR_MESSAGE);
-            }
-        }
+        Function<Integer, Subject> subjectMapper = Subject::getOptionalSubjectByOrdinal;
+        return readSubjects(this::validateOptionalSubject, subjectMapper,
+            OPTIONAL_SUBJECT_ERROR_MESSAGE);
     }
 
     private int validateManageMenuRange(int manageMenuNumber) {
-        if (INPUT_START_RANGE <= manageMenuNumber && manageMenuNumber <= MANAGE_MENU_END_RANGE) {
-            return manageMenuNumber;
-        }
-        throw new IllegalArgumentException();
+        return validateRange(manageMenuNumber, MANAGE_MENU_END_RANGE);
     }
 
     private int validateManageStudentRange(int manageStudentNumber) {
-        if (INPUT_START_RANGE <= manageStudentNumber
-            && manageStudentNumber <= MANAGE_STUDENT_END_RANGE) {
-            return manageStudentNumber;
-        }
-        throw new IllegalArgumentException();
+        return validateRange(manageStudentNumber, MANAGE_STUDENT_END_RANGE);
     }
 
     private int validateManageScoreRange(int manageScoreNumber) {
-        if (INPUT_START_RANGE <= manageScoreNumber && manageScoreNumber <= MANAGE_SCORE_END_RANGE) {
-            return manageScoreNumber;
+        return validateRange(manageScoreNumber, MANAGE_SCORE_END_RANGE);
+    }
+
+    private void validateIsDuplicate(List<String> subjectNumbers) {
+        if (!(subjectNumbers.stream().distinct().count() == subjectNumbers.size())) {
+            throw new IllegalArgumentException();
         }
-        throw new IllegalArgumentException();
     }
 
     private void validateName(String input) {
-        if (!NAME_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException();
-        }
+        validatePattern(NAME_PATTERN, input);
     }
 
     private void validateRequireSubject(String input) {
-        if (!REQUIRE_SUBJECT_PATTERN.matcher(input).matches()) {
-            throw new IllegalArgumentException();
-        }
+        validatePattern(REQUIRE_SUBJECT_PATTERN, input);
     }
 
-//    private int validateStateRange(int stateNumber) {
-//        if (INPUT_START_RANGE <= stateNumber && stateNumber <= STATE_END_RANGE) {
-//            return stateNumber;
-//        }
-//        throw new IllegalArgumentException();
-//    }
-
-    private String readUserNameInput() {
-        Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
+    private void validateOptionalSubject(String input) {
+        validatePattern(OPTIONAL_SUBJECT_PATTERN, input);
     }
 
-    private String readSubjectInput() {
+    private String readLineInput() {
         Scanner sc = new Scanner(System.in);
         return sc.nextLine();
     }
